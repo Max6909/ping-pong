09ce157e96f32a63eac56e321f9f38a288d6c4fc
Max6909 <mugasik@gmail.com>
2026-05-24 11:26:06 +0300

    new class

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ HEIGHT = 900
 FPS = 120
 game = True
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, image_, width, height, x, y):
+        self.image = transform.scale(image.load(image_), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
 timer = time.Clock()
 
 win = display.set_mode((WIDTH, HEIGHT))
